025f113ca31847675e9fe2e8664d173230eb4eef
matias angelaccio <matiasangelaccio03@gmail.com>
2023-05-22 05:27:28 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿Console.WriteLine("introduce tu nombre");
 string nombre = Console.ReadLine();
 Console.WriteLine("Hola " + nombre);
+
+// hora?
+Console.WriteLine("introduce la hora");
+string hora = Console.ReadLine();
+Console.WriteLine("la hora es  " + hora);
