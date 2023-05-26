ba1ce352df9f8a83f014b7bee69428bb6c1e7393
matias angelaccio <matiasangelaccio03@gmail.com>
2023-05-26 07:25:50 -0300

    segundoejercicio

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("introduce tu nombre");
+﻿/*Console.WriteLine("introduce tu nombre");
 string nombre = Console.ReadLine();
 Console.WriteLine("Hola " + nombre);
 
@@ -6,3 +6,33 @@ Console.WriteLine("Hola " + nombre);
 Console.WriteLine("introduce la hora");
 string hora = Console.ReadLine();
 Console.WriteLine("la hora es  " + hora);
+*/
+/*
+int i = 0;
+long j  = 1;
+float f = 1.5555f;
+double g = '2';
+char c = 'c';
+string str = "string";
+
+Console.Write("int: " + i);
+Console.WriteLine("long: " + j);
+
+
+Console.Write("floar: " + f);
+Console.WriteLine("double: " + g);
+
+
+Console.Write("char: " + c);
+Console.WriteLine("string: " + str);
+*/
+
+// NOMBRE
+Console.WriteLine("introduce tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("introce tu apellido");
+string apellido = Console.ReadLine();
+
+Console.WriteLine("¿sabes programar?");
+string program = Console.ReadLine();
+Console.WriteLine("Hola " + nombre + apellido + program);
